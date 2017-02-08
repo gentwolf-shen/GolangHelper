456f0a96f516c5e55307325b604b1e66faed4f1a
jerry <smg@sightp.com>
2017-02-08 19:11:22 +0800

    拆分实现工具类方法

--- src/gentwolf/GolangHelper/util/util.go
+++ src/gentwolf/GolangHelper/util/util.go
@@ -1,52 +1,13 @@
 package util
 
 import (
-	"crypto/md5"
 	"crypto/rand"
-	"encoding/base64"
 	"encoding/hex"
-	"fmt"
 	"math"
 	rnd "math/rand"
-	"strconv"
 	"strings"
 	"time"
 )
-
-func ToInt(str string) int {
-	i, _ := strconv.Atoi(str)
-	return i
-}
-
-func ToInt64(str string) int64 {
-	i, _ := strconv.ParseInt(str, 10, 64)
-	return i
-}
-
-func ToFloat32(str string) float32 {
-	i, _ := strconv.ParseFloat(str, 64)
-	return float32(i)
-}
-
-func ToFloat64(str string) float64 {
-	i, _ := strconv.ParseFloat(str, 64)
-	return i
-}
-
-func ToStr(n interface{}, args ...int) string {
-	if len(args) == 0 {
-		return fmt.Sprintf("%d", n)
-	} else {
-		format := "%." + fmt.Sprintf("%d", args[0]) + "f"
-		return fmt.Sprintf(format, n)
-	}
-}
-
-func Md5(str string) string {
-	h := md5.New()
-	h.Write([]byte(str))
-	return hex.EncodeToString(h.Sum(nil))
-}
 
 func RndStr(length int) string {
 	r := rnd.New(rnd.NewSource(time.Now().UnixNano()))
@@ -83,23 +44,6 @@ func SubString(str string, start, length int) string {
 
 	s := rs[start:end]
 	return string(s)
-}
-
-func Base64DecodeToByte(str string) ([]byte, error) {
-	return base64.RawStdEncoding.DecodeString(str)
-}
-
-func Base64EncodeByte(b []byte) string {
-	return base64.StdEncoding.EncodeToString(b)
-}
-
-func Base64Decode(str string) string {
-	b, _ := base64.RawStdEncoding.DecodeString(str)
-	return string(b)
-}
-
-func Base64Encode(str string) string {
-	return base64.StdEncoding.EncodeToString([]byte(str))
 }
 
 func Ceil(size, count int) int {

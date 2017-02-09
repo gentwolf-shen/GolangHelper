d346c3ffd9df89bab358cab840b0b7b7f57c0daa
jerry <smg@sightp.com>
2017-02-09 14:38:39 +0800

    内部实现调整

--- src/gentwolf/GolangHelper/view/view.go
+++ src/gentwolf/GolangHelper/view/view.go
@@ -1,14 +1,9 @@
-// 模板类
-// 扩展了html/template，支持layout功能，增加缓存功能，定时检测模板根目录中的状态文件，判断是否需要清除缓存。
-
 package view
 
 import (
 	"html/template"
 	"io"
-	"os"
 	"strings"
-	"time"
 )
 
 var (
@@ -18,31 +13,20 @@ var (
 	tplExt  string
 )
 
-//在模板根目录中检测是否有“status.txt”文件，如果有，则清除缓存。
-func checkStatus() {
-	for _ = range time.Tick(10 * time.Second) {
-		status := tplPath + "status.txt"
-		if _, err := os.Stat(status); err == nil {
-			for k, _ := range tpls {
-				delete(tpls, k)
-			}
-			os.Remove(status)
-		}
-	}
-}
-
 //设置模板根目录，及后缀。
 func SetConfig(path, ext string) {
-	if !strings.HasSuffix(tplPath, "/") {
-		tplPath += "/"
+	if !strings.HasSuffix(path, "/") {
+		path += "/"
+	}
+
+	if !strings.HasPrefix(ext, ".") {
+		ext = "." + ext
 	}
 
 	tplPath = path
 	tplExt = ext
 
 	tpls = make(map[string]*template.Template)
-
-	go checkStatus()
 }
 
 //输出模板

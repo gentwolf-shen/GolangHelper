6d584139a16d406564058818c07aaafc0848ec24
jerry <smg@sightp.com>
2019-01-17 16:53:18 +0800

    更新邮件发送实现

--- src/gentwolf/GolangHelper/email/email.go
+++ src/gentwolf/GolangHelper/email/email.go
@@ -7,20 +7,29 @@ import (
 	"fmt"
 	"io/ioutil"
 	"mime"
+	"net/mail"
 	"net/smtp"
 	"path/filepath"
 	"strings"
 	"time"
 )
 
+// Attachment represents an email attachment.
 type Attachment struct {
 	Filename string
 	Data     []byte
 	Inline   bool
 }
 
+// Header represents an additional email header.
+type Header struct {
+	Key   string
+	Value string
+}
+
+// Message represents a smtp message.
 type Message struct {
-	From            string
+	From            mail.Address
 	To              []string
 	Cc              []string
 	Bcc             []string
@@ -28,6 +37,7 @@ type Message struct {
 	Subject         string
 	Body            string
 	BodyContentType string
+	Headers         []Header
 	Attachments     map[string]*Attachment
 }
 
@@ -48,6 +58,7 @@ func (m *Message) attach(file string, inline bool) error {
 	return nil
 }
 
+// AttachBuffer attaches a binary attachment.
 func (m *Message) AttachBuffer(filename string, buf []byte, inline bool) error {
 	m.Attachments[filename] = &Attachment{
 		Filename: filename,
@@ -57,12 +68,21 @@ func (m *Message) AttachBuffer(filename string, buf []byte, inline bool) error {
 	return nil
 }
 
+// Attach attaches a file.
 func (m *Message) Attach(file string) error {
 	return m.attach(file, false)
 }
 
+// Inline includes a file as an inline attachment.
 func (m *Message) Inline(file string) error {
 	return m.attach(file, true)
+}
+
+// Ads a Header to message
+func (m *Message) AddHeader(key string, value string) Header {
+	newHeader := Header{Key: key, Value: value}
+	m.Headers = append(m.Headers, newHeader)
+	return newHeader
 }
 
 func newMessage(subject string, body string, bodyContentType string) *Message {
@@ -73,14 +93,17 @@ func newMessage(subject string, body string, bodyContentType string) *Message {
 	return m
 }
 
+// NewMessage returns a new Message that can compose an email with attachments
 func NewMessage(subject string, body string) *Message {
 	return newMessage(subject, body, "text/plain")
 }
 
+// NewHTMLMessage returns a new Message that can compose an HTML email with attachments
 func NewHTMLMessage(subject string, body string) *Message {
 	return newMessage(subject, body, "text/html")
 }
 
+// Tolist returns all the recipients of the email
 func (m *Message) Tolist() []string {
 	tolist := m.To
 
@@ -95,20 +118,24 @@ func (m *Message) Tolist() []string {
 	return tolist
 }
 
+// Bytes returns the mail data
 func (m *Message) Bytes() []byte {
 	buf := bytes.NewBuffer(nil)
 
-	buf.WriteString("From: " + m.From + "\r\n")
+	buf.WriteString("From: " + m.From.String() + "\r\n")
 
 	t := time.Now()
-	buf.WriteString("Date: " + t.Format(time.RFC822) + "\r\n")
+	buf.WriteString("Date: " + t.Format(time.RFC1123Z) + "\r\n")
 
 	buf.WriteString("To: " + strings.Join(m.To, ",") + "\r\n")
 	if len(m.Cc) > 0 {
 		buf.WriteString("Cc: " + strings.Join(m.Cc, ",") + "\r\n")
 	}
 
-	buf.WriteString("Subject: " + m.Subject + "\r\n")
+	//fix  Encode
+	var coder = base64.StdEncoding
+	var subject = "=?UTF-8?B?" + coder.EncodeToString([]byte(m.Subject)) + "?="
+	buf.WriteString("Subject: " + subject + "\r\n")
 
 	if len(m.ReplyTo) > 0 {
 		buf.WriteString("Reply-To: " + m.ReplyTo + "\r\n")
@@ -116,11 +143,18 @@ func (m *Message) Bytes() []byte {
 
 	buf.WriteString("MIME-Version: 1.0\r\n")
 
+	// Add custom headers
+	if len(m.Headers) > 0 {
+		for _, header := range m.Headers {
+			buf.WriteString(fmt.Sprintf("%s: %s\r\n", header.Key, header.Value))
+		}
+	}
+
 	boundary := "f46d043c813270fc6b04c2d223da"
 
 	if len(m.Attachments) > 0 {
 		buf.WriteString("Content-Type: multipart/mixed; boundary=" + boundary + "\r\n")
-		buf.WriteString("--" + boundary + "\r\n")
+		buf.WriteString("\r\n--" + boundary + "\r\n")
 	}
 
 	buf.WriteString(fmt.Sprintf("Content-Type: %s; charset=utf-8\r\n\r\n", m.BodyContentType))
@@ -146,7 +180,10 @@ func (m *Message) Bytes() []byte {
 					buf.WriteString("Content-Type: application/octet-stream\r\n")
 				}
 				buf.WriteString("Content-Transfer-Encoding: base64\r\n")
-				buf.WriteString("Content-Disposition: attachment; filename=\"" + attachment.Filename + "\"\r\n\r\n")
+
+				buf.WriteString("Content-Disposition: attachment; filename=\"=?UTF-8?B?")
+				buf.WriteString(coder.EncodeToString([]byte(attachment.Filename)))
+				buf.WriteString("?=\"\r\n\r\n")
 
 				b := make([]byte, base64.StdEncoding.EncodedLen(len(attachment.Data)))
 				base64.StdEncoding.Encode(b, attachment.Data)
@@ -169,18 +206,37 @@ func (m *Message) Bytes() []byte {
 	return buf.Bytes()
 }
 
+// Send sends the message.
+func SendSimple(addr string, auth smtp.Auth, m *Message) error {
+	return smtp.SendMail(addr, auth, m.From.Address, m.Tolist(), m.Bytes())
+}
+
 func Send(config *Config, m *Message) error {
 	auth := smtp.PlainAuth("", config.From, config.Password, config.Smtp)
-	m.From = config.From
+	m.From = mail.Address{Name: strings.Split(config.From, "@")[0], Address: config.From}
 	m.To = config.To
-	return smtp.SendMail(config.Smtp+":"+config.Port, auth, m.From, m.Tolist(), m.Bytes())
+	return SendSimple(config.Smtp+":"+config.Port, auth, m)
 }
 
 func SendTo(config *Config, toEmails []string, m *Message) error {
 	auth := smtp.PlainAuth("", config.From, config.Password, config.Smtp)
-	m.From = config.From
+	m.From = mail.Address{Name: strings.Split(config.From, "@")[0], Address: config.From}
 	m.To = toEmails
-	return smtp.SendMail(config.Smtp+":"+config.Port, auth, m.From, m.Tolist(), m.Bytes())
+	return SendSimple(config.Smtp+":"+config.Port, auth, m)
+}
+
+func SendV2(config *ConfigV2, m *Message) error {
+	auth := smtp.PlainAuth("", config.FromAddress, config.Password, config.Smtp)
+	m.From = mail.Address{Name: config.FromName, Address: config.FromAddress}
+	m.To = config.To
+	return SendSimple(config.Smtp+":"+config.Port, auth, m)
+}
+
+func SendToV2(config *ConfigV2, toEmails []string, m *Message) error {
+	auth := smtp.PlainAuth("", config.FromAddress, config.Password, config.Smtp)
+	m.From = mail.Address{Name: config.FromName, Address: config.FromAddress}
+	m.To = toEmails
+	return SendSimple(config.Smtp+":"+config.Port, auth, m)
 }
 
 func LoadConfig(filename string) (*Config, error) {
@@ -200,4 +256,13 @@ type Config struct {
 	Smtp     string   `json:"smtp"`
 	Port     string   `json:"port"`
 	To       []string `json:"to"`
+}
+
+type ConfigV2 struct {
+	FromName    string   `json:"name"`
+	FromAddress string   `json:"address"`
+	Password    string   `json:"password"`
+	Smtp        string   `json:"smtp"`
+	Port        string   `json:"port"`
+	To          []string `json:"to"`
 }
